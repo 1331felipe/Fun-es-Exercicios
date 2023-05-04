2d66fd1b89f6df560820190947a63901da651433
1331felipe <felipehuh2023@gmail.com>
2023-05-03 21:21:06 -0300

    Funções-Exercicios

---
+++ index.js
@@ -0,0 +1,37 @@
+// function calcularArea(numAlt, numLar) {
+//   const altura = numAlt;
+//   const largura = numLar;
+//   const area = altura * largura;
+//   return area;
+// }
+
+// console.log(calcularArea());
+
+// function printHelloWorld() {
+//   console.log("Hello, world.");
+// }
+// printHelloWorld();
+
+// function nameAsk(theName) {
+//   console.log("Hello ", theName);
+// }
+// nameAsk("Fabio");
+// nameAsk("Julia");
+// nameAsk("Carlos");
+
+// function sum(number1, number2) {
+//   const result = number1 + number2;
+//   return result;
+// }
+// console.log(sum(3, 4));
+// console.log(sum(2, 5));
+
+// const array = [4, 5, 6, 7, 8];
+
+// function array2Number(number1, number2) {
+//   number1 = array[0];
+//   number2 = array[4];
+//   const division = number1 / number2;
+//   return division;
+// }
+// console.log(array2Number());
